aaef8504215311ee5f546cbe8a16d6c30934feae
zero <747050097@qq.com>
2022-10-07 10:46:50 +0800

    error_handling errors1.rs

--- exercises/strings/strings3.rs
+++ exercises/strings/strings3.rs
@@ -1,21 +1,24 @@
 // strings3.rs
 // Execute `rustlings hint strings3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
 
 fn trim_me(input: &str) -> String {
     // TODO: Remove whitespace from both ends of a string!
-    ???
+    let mut output = String::from(input);
+    output.trim().to_string()
 }
 
 fn compose_me(input: &str) -> String {
     // TODO: Add " world!" to the string! There's multiple ways to do this!
-    ???
+    let mut output = String::from(input);
+    output.push_str(" world!");
+    output
 }
 
 fn replace_me(input: &str) -> String {
     // TODO: Replace "cars" in the string with "balloons"!
-    ???
+    let mut output = String::from(input);
+    output.replace("cars", "balloons").to_string()
 }
 
 #[cfg(test)]
